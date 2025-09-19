809740bf7989ecd692de1462dc4b9b0026a969f2
bsinek <bsinek2024@gmail.com>
2025-09-19 05:24:22 -0400

    feat(playbar): add non-functional progress bar

--- src/components/Playbar.jsx
+++ src/components/Playbar.jsx
@@ -6,25 +6,28 @@ export const Playbar = () => {
             <div className="w-[30%]">
                 CURRENT SECTION HERE
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col flex-1 gap-2">
                 <div className="flex justify-center items-center gap-4 h-8">
                     <button className="h-8 w-8 p-2 text-light-grey hover:text-white hover:scale-105 transition-all duration-75">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-full">
                             <path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7z"/>
                         </svg>
                     </button>
-
                     <button className="h-8 w-8 p-2 rounded-full bg-current hover:scale-105 transition-transform duration-75">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-full text-black">
                             <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288z"/>
                         </svg>
                     </button>
-
                     <button className="h-8 w-8 p-2 text-light-grey hover:text-white hover:scale-105 transition-all duration-75">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-full rotate-180">
                             <path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7z"/>
                         </svg>
                     </button>
+                </div>
+                <div className="flex h-4 items-center justify-center gap-2 text-light-grey text-xs tabular-nums">
+                    <div className="w-12 text-right">0:00</div>
+                    <div className="h-1/4 flex-1 bg-[#4d4c4c] rounded-xs"></div>
+                    <div className="w-12 text-left">0:00</div>
                 </div>
             </div>
             <div className="w-[30%] flex justify-end">
