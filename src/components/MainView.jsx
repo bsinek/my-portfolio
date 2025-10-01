389681e5ddf97f3f6f8ecfc0fcdf09aa9cd0e1b3
bsinek <bsinek2024@gmail.com>
2025-10-01 03:28:45 -0400

    feat(main-view): add About Me section with dummy controls and increase duration-75 to duration-100 for playbar buttons

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react"
 import { HorizontalScroll } from "./HorizontalScroll"
+import { Lorem } from "../Lorem"
 
 const MainViewHeader = ({ img, position, size, opacity }) => {
     return (
@@ -31,9 +32,31 @@ const AboutSection = () => {
         <div className="relative bg-spotify-grey">
             {/* gradient */}
             <div className="absolute inset-0 h-44 bg-gradient-to-b from-zinc-800 to-transparent" />
+            {/* buttons */}
+            <div className="relative h-24 p-6 flex gap-8 items-center">
+                <button className="h-full aspect-square p-4 rounded-full bg-spotify-green hover:scale-105 transition-transform duration-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-full text-black">
+                        <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288z"/>
+                    </svg>
+                </button>
+                <button className="flex justify-center items-center h-8 rounded-full border-1 border-[#7c7c7c] p-4 hover:scale-[103%] hover:border-white transition-all duration-100">
+                    <p className="text-sm">Do Not Press</p>
+                </button>
+            </div>
             {/* content */}
-            <div className="relative p-12">
-                
+            <div className="relative grid grid-cols-2 h-48 px-6 gap-6">
+                <div>
+                    <h2 className="text-2xl font-semibold mb-4">About me</h2>
+                    <div className="px-4 text-lg">
+                        <Lorem/>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="text-2xl font-semibold mb-4">Artist pick</h2>
+                    <div className="h-72 flex justify-center items-center border rounded-2xl">
+                        THINGY HERE
+                    </div>
+                </div>
             </div>
         </div>
     )
