78e40f27b5d3e5d4b4f498436aa176f38bbcb880
bsinek <bsinek2024@gmail.com>
2025-10-05 13:33:44 -0400

    refactor(sidebar): change "Quick Links" div to h3 element

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ export const Sidebar = () => {
         <aside className="group/sidebar w-[420px] h-full rounded-lg bg-spotify-grey">
             <div className="p-4 relative flex items-center overflow-hidden justify-between">
                 <i className="bi bi-layout-sidebar text-light-grey text-lg absolute opacity-0 -translate-x-8 group-hover/sidebar:opacity-100 group-hover/sidebar:translate-x-0 transition-all duration-200 group-hover/sidebar:delay-[50ms]"></i>
-                <span className="ml-1 font-semibold transition-transform duration-200 group-hover/sidebar:translate-x-6 delay-[50ms] group-hover/sidebar:delay-0">Quick Links</span>
+                <h3 className="ml-1 font-semibold transition-transform duration-200 group-hover/sidebar:translate-x-6 delay-[50ms] group-hover/sidebar:delay-0">Quick Links</h3>
                 <i className="bi bi-link-45deg text-xl mr-1.5"></i>
             </div>
             <div className="px-2 h-full">
