423cd2081abf41a13b1fad623e811ae61dec7c2e
bsinek <bsinek2024@gmail.com>
2026-01-09 03:31:33 -0800

    feat(projects): add embla carousel with rough styles

--- src/components/MainView/Projects.jsx
+++ src/components/MainView/Projects.jsx
@@ -1,7 +1,15 @@
+import EmblaCarousel from "./EmblaCarousel"
+import "./embla.css"
+
 export const Projects = () => {
+    const slides = [0, 1, 2, 3, 4]
     return (
         <section className="h-mainview">
-
+            <h2 className="text-4xl font-semibold mb-8">Projects</h2>
+            <EmblaCarousel
+                slides={slides}
+                options={{ loop: true, align: "center" }}
+            />
         </section>
     )
 }
