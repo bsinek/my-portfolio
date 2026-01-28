ff7798a4b7fcaa90f1bc8daae2883439738a0022
bsinek <bsinek2024@gmail.com>
2026-01-28 04:07:03 -0500

    fix: increase main content min width

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
         {LAYOUT.showSidebar && <div className={`col-span-1 overflow-hidden ${rounded} ${sidebarColor}`}>
           <Sidebar activeSection={activeSection} />
         </div>}
-        <div ref={mainContentRef} className={`col-span-1 min-h-0 min-w-3xl overflow-hidden ${rounded}`}>
+        <div ref={mainContentRef} className={`col-span-1 min-h-0 min-w-4xl overflow-hidden ${rounded}`}>
           <MainView 
             setActiveSection={setActiveSection}
             progressMV={progressMV}
