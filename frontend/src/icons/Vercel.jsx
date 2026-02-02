40b218944e61fc326379cd5d1b29ecb845fe402b
bsinek <bsinek2024@gmail.com>
2026-02-02 17:28:59 -0500

    fix(icons): fix Vercel icon kebab case

--- frontend/src/icons/Vercel.jsx
+++ frontend/src/icons/Vercel.jsx
@@ -1,9 +1,9 @@
 export default function VercelIcon() {
     return (
-        <svg data-testid="geist-icon" stroke-linejoin="round" viewBox="0 0 16 16" fill="white">
-            <g clip-path="url(#clip0_872_3186)">
-                <circle cx="8" cy="8" r="7.25" fill="black" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></circle>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4.5L11.5 10.625H4.5L8 4.5Z" fill="var(--ds-gray-100)"></path>
+        <svg data-testid="geist-icon" strokeLinejoin="round" viewBox="0 0 16 16" fill="white">
+            <g clipPath="url(#clip0_872_3186)">
+                <circle cx="8" cy="8" r="7.25" fill="black" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></circle>
+                <path fillRule="evenodd" clipRule="evenodd" d="M8 4.5L11.5 10.625H4.5L8 4.5Z" fill="var(--ds-gray-100)"></path>
             </g>
             <defs>
                 <clipPath id="clip0_872_3186">
