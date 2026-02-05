717a0674c6aff6f11fdd1792e7a3b67b42cc1457
bsinek <bsinek2024@gmail.com>
2026-02-04 22:07:06 -0500

    feat(backend): implement Project and Skill schemas and register to admin

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    techstack = models.ManyToManyField(Skill, blank=True)
+    year = models.IntegerField()
+    link = models.URLField()
+
+    # image paths
+    preview = models.CharField(max_length=300)
+    icon = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
