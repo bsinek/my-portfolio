60d4a43c13ed05c26e12e3bb8e747c7fa1d9db6e
bsinek <bsinek2024@gmail.com>
2026-02-13 14:29:32 -0500

    feat(backend/api): expose api routes in backend for easier navigation

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'portfolio'
+
 urlpatterns = [
-    path("experiences/", views.experience_list),
-    path("projects/", views.projects_list),
-    path("skills/", views.category_list),
-    path("resume/", views.resume_detail),
+    path("", views.api_root, name='api-root'),
+    path("experiences/", views.experience_list, name='experience-list'),
+    path("projects/", views.projects_list, name='project-list'),
+    path("skills/", views.category_list, name='skill-list'),
+    path("resume/", views.resume_detail, name='resume-detail'),
 ]
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,7 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Experience, Project, Category, Resume
 from .serializers import ExperienceSerializer, ProjectSerializer, CategorySerializer, ResumeSerializer
+
+@api_view(["GET"])
+def api_root(request):
+    # exposes all api routes
+    return Response({
+        'experiences': reverse('portfolio:experience-list', request=request),
+        'projects': reverse('portfolio:project-list', request=request),
+        'skills': reverse('portfolio:skill-list', request=request),
+        'resume': reverse('portfolio:resume-detail', request=request),
+    })
 
 @api_view(["GET"])
 def experience_list(request):
