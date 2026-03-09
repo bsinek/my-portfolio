3c41d156af16f76815fca6c1897795bc37e3d61c
bsinek <bsinek2024@gmail.com>
2026-03-08 22:47:27 -0400

    feat: add maintenance toggle and screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { MainView } from "./components/MainView/MainView"
 import { Playbar } from "./components/Playbar/Playbar"
 import { LoadingScreen } from "./components/LoadingScreen/LoadingScreen"
 import { LAYOUT } from "./config/general"
+import { MAINTENANCE } from "./config/general"
 import { useMotionValue, AnimatePresence } from "motion/react"
 
 function App() {
@@ -44,6 +45,19 @@ function App() {
       <AnimatePresence>
         {showLoading && <LoadingScreen />}
       </AnimatePresence>
+
+      {MAINTENANCE && <div className="absolute z-10 bg-spotify-grey inset-0 flex justify-center items-center flex-col gap-4">
+        <p className="font-extralight tracking-wide text-3xl text-center">
+          Site is being updated.
+          <br/>
+          Check back later.
+        </p>
+        <div className="h-12 aspect-square rounded-full bg-white p-2">
+          <svg fill="currentColor" className="bi bi-cone text-black" viewBox="0 0 16 16">
+            <path d="m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9s-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12m-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4s1.2-.036 1.725-.098m4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257z"/>
+          </svg>
+        </div>
+      </div>}
 
       <div className={`h-screen grid ${gridCols} ${gridRows} ${padding} ${gap}`}>
         {LAYOUT.showSidebar && <div className={`col-span-1 overflow-hidden ${rounded} ${sidebarColor}`}>
